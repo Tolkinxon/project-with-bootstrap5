d278471b96b3f462b40d4189e165fc857953c938
Tolkinxon <krosstony639@gmail.com>
2023-10-28 21:05:30 +0300

    all clens

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-function positiveSum(arr) {
-  let sum = 0
- for(let i = 0; i < arr.length; i++){
-    sum = sum + (arr[i] >= 0 ? arr[i] : 0 )
- }
-
-  return sum
-}
-
-const array = [1,2,3,4,-23,0,5]
-
-console.log(positiveSum(array));
-
-
+'use stricct'
