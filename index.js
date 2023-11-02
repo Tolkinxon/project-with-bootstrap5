3814c4cdd6bd9872b14300e5d3b7d872fdc2ec76
Tolkinxon <krosstony639@gmail.com>
2023-11-02 22:07:39 +0300

    some changes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const path = require('path')
+// const path = require("path");
+
+// const fs = require("fs")
 
 
 // console.log(__filename);
@@ -9,4 +11,28 @@ const path = require('path')
 // console.log(path.dirname(__filename));
 // console.log(path.dirname(__dirname));
 // console.log(path.extname(__filename));
-console.log(path.extname(__dirname));
+// console.log(path.extname(__dirname));
+// console.log(path.parse(__filename).base);
+// console.log(path.join( 'backend','forntend', 'index.html'));
+// console.log(path.resolve('backend',"./frontend",'index.html'));
+"scissors", "paper","rock"
+
+
+const rps = (p1, p2) => {
+    const w = p1 + p2
+
+    if(
+        w[0] === 's' && w[w.length - 1 ] === 'r' ||
+        w[0] === 'p' && w[w.length - 1] === 'k' || 
+        w[0] === 'r' && w[w.length - 1] === 's'
+       ){
+        return '"Player 1 won!"'
+    }
+
+};
+
+
+
+console.log(rps("scissors", "paper"));
+https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/javascript
+
