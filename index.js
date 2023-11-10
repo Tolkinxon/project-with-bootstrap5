e87d8b5fcebb0fb97b4f43fcaf9cac494fba08af
Tolkinxon <krosstony639@gmail.com>
2023-11-10 19:36:44 +0300

    some changes

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-function firstNonRepeatingLetter(s) {
-    // Add your code here
-    const arr = [...s]
-
-    const arr2 = arr.filter(item => {
-               
-               return arr.filter( thing => { return item.toLowerCase() === thing.toLowerCase() }  ).length === 1 
-
-             
-            })[0] 
-            
-    return arr2 === undefined ? '' : arr2
+function add(x){
+    let sum = x;
+    function resultFn(y){
+        sum += y;
+        return resultFn;
+    }
+    resultFn.valueOf = function(){
+            return sum;
+        };
+    return resultFn;
 }
+
+console.log(add(2)(2)(2).valueOf());
+
+
+
 
