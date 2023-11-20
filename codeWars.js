441c8fb748f5c749e8472787a8e5fdb776461030
Tolkinxon <krosstony639@gmail.com>
2023-11-20 03:54:30 +0300

    some changes

--- codeWars.js
+++ codeWars.js
@@ -1,7 +1,39 @@
 snail = function(array) {
-  // enjoy
+
+    let returningArr = array[0]
+
+    array[0] = 0
+
+    let newArr = array.filter(item => item !== 0)
+
+    for(let i = 0; i < newArr.length; i++){
+      returningArr[returningArr.length] = newArr[i][newArr[i].length - 1]
+      newArr[i] = newArr[i].splice(0, newArr[i].length - 1)
+    }
+
+    returningArr = [...returningArr, ...newArr[newArr.length - 1].reverse()] 
+
+    newArr.slice(0, 0)
+
+    
+    console.log(newArr);
+
+    
+    
+
 }
 
 
 
 //https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/javascript
+
+array = [[1,2,3],
+         [8,9,4],
+         [7,6,5]]
+
+
+// snail(array)
+
+console.log(array.slice(0, array.length - 1));
+
+ 
