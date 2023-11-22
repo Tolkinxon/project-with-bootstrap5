9a65369a3f9e4c33a31dc1b77b3a86fabdfe11c1
Tolkinxon <krosstony639@gmail.com>
2023-11-23 02:21:35 +0300

    making new array

--- codeWars.js
+++ codeWars.js
@@ -1,21 +1,26 @@
 snail = function(array) {
 
+
+//  taking firth row and cleaning this row
     let returningArr = array[0]
-
     array[0] = 0
-
     let newArr = array.filter(item => item !== 0)
 
+// taking last item each array and clearing them
     for(let i = 0; i < newArr.length; i++){
       returningArr[returningArr.length] = newArr[i][newArr[i].length - 1]
       newArr[i] = newArr[i].splice(0, newArr[i].length - 1)
     }
 
+// taking last array by reverse and clearing it
     returningArr = [...returningArr, ...newArr[newArr.length - 1].reverse()] 
     newArr = newArr.filter((item, ind) => ind !== newArr.length - 1)
 
-    console.log(newArr);
-
+// taking firth item each array 
+  for(let i = 0; i < newArr.length; i++){
+    returningArr[returningArr.length] = newArr[i][newArr[i].length - 1]
+    newArr[i] = newArr[i].splice(0, newArr[i].length - 1)
+  }
 
    
 
