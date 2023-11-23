73b58883d80571bce2c0c25e6e9aebccc1bdf428
Tolkinxon <krosstony639@gmail.com>
2023-11-23 03:27:03 +0300

    missia completed

--- codeWars.js
+++ codeWars.js
@@ -1,5 +1,8 @@
 snail = function(array) {
 
+if(array.length === 1){
+  return array[0]
+}
 
 //  taking firth row and cleaning this row
     let returningArr = array[0]
@@ -12,11 +15,17 @@ snail = function(array) {
       newArr[i] = newArr[i].splice(0, newArr[i].length - 1)
     }
 
-
 // taking last array by reverse and clearing it
-  returningArr = [...returningArr, ...newArr[newArr.length - 1].reverse()] 
-  
-  newArr = newArr.filter((item, ind) => ind !== newArr.length - 1)
+  if(newArr.length === 1){
+    returningArr = [...returningArr, ...newArr[0]]
+    return returningArr
+  }
+  else{
+    returningArr = [...returningArr, ...newArr[newArr.length - 1].reverse()] 
+    newArr = newArr.filter((item, ind) => ind !== newArr.length - 1)
+  }
+
+
 
 
 // taking firth item each array 
@@ -26,29 +35,24 @@ snail = function(array) {
   }
 
 
-  
-  console.log(snail(newArr));
-
-
 
   if(newArr.length === 1){
     returningArr = [...returningArr, ...newArr[0]]
     return returningArr
   }
-
-
-    
-
+  else{
+    return [...returningArr,...snail(newArr)]
+  }
 }
 
 
 
 //https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/javascript
 
-array = [[1,2,3,4],
-         [12,13,14,5],
-         [11,16,15,6],
-         [10,9,8,7]]
+// array = [[1,2,3,4],
+//          [12,13,14,5],
+//          [11,16,15,6],
+//          [10,9,8,7]]
 
 // array = [[1,2,3],
 //          [8,9,4],
@@ -57,11 +61,14 @@ array = [[1,2,3,4],
 // array = [[1,2],
 //          [4,3]]
 
+
+array = [[1]]
+
 // snail(array)
 
 
 // console.log(snail(array));
-snail(array)
+console.log(snail(array));
 
 
  
