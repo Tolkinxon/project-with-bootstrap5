c6df903f4ccbaa0a493052b77dfbb64617be4ce6
Tolkinxon <krosstony639@gmail.com>
2023-11-29 05:12:02 +0300

    missia completed

--- codeWars.js
+++ codeWars.js
@@ -49,26 +49,6 @@ if(array.length === 1){
 
 //https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/javascript
 
-// array = [[1,2,3,4],
-//          [12,13,14,5],
-//          [11,16,15,6],
-//          [10,9,8,7]]
-
-// array = [[1,2,3],
-//          [8,9,4],
-//          [7,6,5]]
-
-// array = [[1,2],
-//          [4,3]]
-
-
-array = [[1]]
-
-// snail(array)
-
-
-// console.log(snail(array));
-console.log(snail(array));
 
 
  
