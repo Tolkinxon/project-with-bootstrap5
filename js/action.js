717e4ad30b908827e351c25cff235cea5e33f784
Tolkinxon <krosstony639@gmail.com>
2024-04-18 15:35:26 +0300

    some changes

--- js/action.js
+++ js/action.js
@@ -1,25 +1,23 @@
+const devices = document.querySelectorAll('.bg-device')
 
-const elConvertButton = document.querySelector('.conver-btn ')
-const elInput = document.querySelector('.input')
-const elSelect = document.querySelector('.select-currency')
-const elH1 = document.querySelector('.response-text')
+window.addEventListener('click', (e)=> {
 
-const ONE_USD = 12300
-const ONE_RUB = 45600
-const ONE_GBR = 78900
+    const selectedEl = e.target.nodeName
+
+    devices.forEach(item => {
+        if(selectedEl === "LI" || selectedEl === "IMG"){
+            item.classList.add('bg-device--active') 
+        }
+        else {
+            item.classList.remove('bg-device--active') 
+        }
+    })
 
 
-
-
-
-elConvertButton.addEventListener('click', () => {
-    
-const inputValue = +elInput.value
-
-    let result = elSelect.value === 'USD' ? ONE_USD * inputValue :
-                 elSelect.value === 'RUB' ? ONE_RUB * inputValue :
-                 elSelect.value === 'GBR' ? ONE_GBR * inputValue :
-                 'Something went wrong'
-
-    elH1.textContent = inputValue + ' ' + elSelect.value + ' will be convert ' + result + " UZS"
 })
+
+
+
+
+
+
