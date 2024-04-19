b264877af38a646f7105ec5b705f8fe624ba9476
Tolkinxon <krosstony639@gmail.com>
2024-04-19 05:09:14 +0300

    some changes

--- js/action.js
+++ js/action.js
@@ -1,9 +1,27 @@
 const eldevices = document.querySelectorAll('.device-item')
+const wrapper = document.querySelector('.wrapper')
 
 eldevices.forEach(item => {
         item.addEventListener('mousedown', ()=>{
                 eldevices.forEach(item => {item.classList.remove('device-item--active')})
                 item.classList.add('device-item--active')
+
+                const deviceName = item.children[0].id;
+
+                console.log(deviceName);
+
+                if(deviceName === "desktop" ){
+                        wrapper.style.width = '100%'
+                }
+                else if(deviceName === "tablet" ){
+                        wrapper.style.width = '768px'
+                }
+                else if(deviceName === "mobile" ){
+                        wrapper.style.width = '375px'
+                }
+                else {
+                        console.log('something went wrong');
+                }
         })
 })
 
