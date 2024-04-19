bcdeba85de689ffeaf0fdb58c1105054c5a4d0f7
Tolkinxon <krosstony639@gmail.com>
2024-04-19 04:38:36 +0300

    some changes

--- js/action.js
+++ js/action.js
@@ -1,10 +1,22 @@
-const devices = document.querySelectorAll('.device-item')
+const eldevices = document.querySelectorAll('.device-item')
 
-devices.forEach(item => {
-        item.addEventListener('click', ()=>{
-                devices.forEach(item => {item.classList.remove('device-item--active')})
+eldevices.forEach(item => {
+        item.addEventListener('mousedown', ()=>{
+                eldevices.forEach(item => {item.classList.remove('device-item--active')})
                 item.classList.add('device-item--active')
         })
+})
+
+
+
+
+
+
+
+//tooltip side
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
 
