cbc34eeb72d9b93cc1cd7def11535f511df2640b
Tolkinxon <krosstony639@gmail.com>
2024-04-19 06:16:21 +0300

    some changes

--- js/action.js
+++ js/action.js
@@ -39,6 +39,14 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 
 
 
+//dropdown side
+var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+  return new bootstrap.Dropdown(dropdownToggleEl)
+})
+
+
+
 
 
 
