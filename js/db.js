df406280dfbe1c68a3618c0f43090d579c3b049b
Tolkinxon <krosstony639@gmail.com>
2024-04-20 07:28:59 +0300

    added stars

--- js/db.js
+++ js/db.js
@@ -3,27 +3,31 @@ const  data = [
         id: 1,
         productName: 'Fancy Product',
         productStar: 0,
+        productDiscount: '',
         productCost: '$40.00 - $80.00',
         buttonName: 'View options'
     },
     {
         id: 2,
         productName: 'Special Item',
-        productStar: 0,
-        productCost: '$20.00 $18.00',
+        productStar: 5,
+        productDiscount: '$20.00',
+        productCost: '$18.00',
         buttonName: 'Add to cart'
     },
     {
         id: 3,
         productName: 'Special Item',
-        productStar: 5,
-        productCost: '$50.00 $25.00',
+        productStar: 0,
+        productDiscount: '$50.00',
+        productCost: '$25.00',
         buttonName: 'Add to cart'
     },
     {
         id: 4,
         productName: 'Popular Item',
-        productStar: 0,
+        productStar: 5,
+        productDiscount: '',
         productCost: '$40.00',
         buttonName: 'Add to cart'
     },
@@ -31,13 +35,15 @@ const  data = [
         id: 5,
         productName: 'Sale Item',
         productStar: 0,
-        productCost: '$50.00 $25.00',
+        productDiscount: '$50.00',
+        productCost: '$25.00',
         buttonName: 'Add to cart'
     },
     {
         id: 6,
         productName: 'Fancy Product',
         productStar: 0,
+        productDiscount: '',
         productCost: '$120.00 - $280.00',
         buttonName: 'View options'
     },
@@ -45,13 +51,15 @@ const  data = [
         id: 7,
         productName: 'Special Item',
         productStar: 5,
-        productCost: '$20.00 $18.00',
+        productDiscount: '$20.00',
+        productCost: '$18.00',
         buttonName: 'Add to cart'
     },
     {
         id: 8,
         productName: 'Popular Item',
         productStar: 5,
+        productDiscount: '',
         productCost: '$40.00',
         buttonName: 'Add to cart'
     },
--- js/workingWithData.js
+++ js/workingWithData.js
@@ -4,6 +4,15 @@ const elCartList = document.querySelector('.list-of-cards__list')
 let elCartItem = ''
 
 data.forEach(item => {
+
+    let star = ``
+
+    console.log(item.productStar);
+
+   for(let i = 0; i < item.productStar; i++){
+    star += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gold" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>`
+   }
+
   let  CartItem  = 
 `      
 <li class="list-of-cards__item list-unstyled col p-0">
@@ -15,9 +24,9 @@ data.forEach(item => {
             <h3 class="h5 fw-bold text-center">
                 ${item.productName}
             </h3>
-            
+            <div class="mb-2 text-center">${star}</div>
             <p class="text-center">
-                ${item.productCost}
+                <del class="me-1 text-secondary">${item.productDiscount}</del>${item.productCost}
             </p>
         </div>
         
@@ -26,10 +35,9 @@ data.forEach(item => {
         </button>
     </div>
 </li>`
-console.log('hello');
    elCartItem += CartItem;
+   star = ''
 })
 
-console.log(elCartItem);
 elCartList.innerHTML = elCartItem
 
